fc0d847c9569153ae0c50d312d573ac951f92392
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2025-12-12 14:16:21 -0600

    feat: Configured system design to use (#1)
    * installed react
    
    * initialiazed shadcn & neobrutalism

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,14 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  site: "https://egamagz.github.io/",
-});
 
+  site: "https://egamagz.github.io/",
+  integrations: [react()],
+});
